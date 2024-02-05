f8f0bc8f84c8bfa03d0ac1c5cfc6c9879df65cf0
kiminhyeong <qhask0330@gmail.com>
2024-02-05 17:52:33 +0900

    랜덤 이미지 가져오기

--- js/request.js
+++ js/request.js
@@ -8,23 +8,29 @@ async function getRequest(url) {
   });
 }
 
-async function getProducts() {
-  const url = `https://yts.mx/api/v2/list_movies.json?limit=5&order_by=asc`;
+async function getProducts(page) {
+  const url = `https://yts.mx/api/v2/list_movies.json?&limit=5&order_by=desc&genre=Animation&page=${page}`;
 
   try {
     // 데이터 요처 및 응답 시도 : 성공일 경우 첫번째 코드 블럭으로 이동
     const data = await getRequest(url);
-    const movieWrapper = document.querySelector('.imgs');
-    console.log(data.data.movies);
-    const movies = data.data.movies;
-    movies.forEach((movie) => {
-      const imgEl = `<img src = ${movie.medium_cover_image}>`;
-      movieWrapper.insertAdjacentHTML('beforeend', imgEl);
-    });
+    const randomData = Math.floor(Math.random() * 5); // 0보다 크고 1보다 작은 난수
+    console.log(data.data.movies[randomData]);
+    const movieWrapper = document.querySelector('.recent-img');
+    const mainMovie = data.data.movies[randomData];
+    const imgEl = `<img src = ${mainMovie.large_cover_image}>`;
+    movieWrapper.insertAdjacentHTML('beforeend', imgEl);
   } catch (error) {
     // 실패할 경우 두번째 코드 블럭으로 이동
     console.log('Error : ', error);
   }
 }
 
-getProducts();
+getProducts(1);
+
+// const numbers = document.querySelectorAll('.numbers span');
+// numbers.forEach((number) => {
+//   number.addEventListener('click', function () {
+//     getProducts(this.textContent);
+//   });
+// });
